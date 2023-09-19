f98818f2e6577a716e990a13bc7488e5b4130404
p1rozh04ek <144774919+p1rozh04ek@users.noreply.github.com>
2023-09-19 09:12:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function step(timestamp) {
     window.requestAnimationFrame(step);
 }
 window.requestAnimationFrame(step);
+
+var snowConfig = {
+  color: [161 / 256, 197 / 256, 231 / 256],
+  count: 100,
+  opacity: 0.8,
+  density: 1 / 50,
+};
