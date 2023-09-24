622c20123a4b38d499de4b66d7eea3ae2504774b
p1rozh04ek <144774919+p1rozh04ek@users.noreply.github.com>
2023-09-24 11:22:47 +0200

    NERD

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-let r = document.querySelector(':root');
-let start;
-function step(timestamp) {
-    if(start === undefined) {
-        start = timestamp;
-    }
+// let r = document.querySelector(':root');
+// let start;
+// function step(timestamp) {
+//     if(start === undefined) {
+//         start = timestamp;
+//     }
 
-    const angle = (timestamp - start) / 30;
-    r.style.setProperty('--gradient-angle', angle + 'deg');
-    window.requestAnimationFrame(step);
-}
-//window.requestAnimationFrame(step);
+//     const angle = (timestamp - start) / 30;
+//     r.style.setProperty('--gradient-angle', angle + 'deg');
+//     window.requestAnimationFrame(step);
+// }
+// window.requestAnimationFrame(step);
 
-var snowConfig = {
-  color: [255, 255, 255],
-  count: 100,
-  opacity: 0.8,
-  density: 1 / 100,
-};
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+// var snowConfig = {
+//   color: [255, 255, 255],
+//   count: 100,
+//   opacity: 0.8,
+//   density: 1 / 100,
+// };
