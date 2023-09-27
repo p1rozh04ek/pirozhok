d0c41a54f9247b40d218abe0693186aebda6cd17
p1rozh04ek <144774919+p1rozh04ek@users.noreply.github.com>
2023-09-27 17:41:49 +0200

    Super nerd with table 4.0

--- index.js
+++ index.js
@@ -12,10 +12,16 @@
 // window.requestAnimationFrame(step);
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
+document.getElementById("date").innerHTML = new Date().toLocaleDateString(navigator.language);
 
-var snowConfig = {
-  color: [255, 255, 255],
-  count: 100,
-  opacity: 0.8,
-  density: 1 / 100,
-};
+function openTechnikum()
+{
+    document.location.href = "http://zsdios.pl/";
+}
+
+// var snowConfig = {
+//   color: [255, 255, 255],
+//   count: 100,
+//   opacity: 0.8,
+//   density: 1 / 100,
+// };
